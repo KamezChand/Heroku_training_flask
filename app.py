949dd76860e68a2005cacbf2306caad4c6d5d51d
kamezchand <kaoz0150chandler@gmail.com>
2019-05-05 11:04:09 +0900

    test1

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from keras.models import load_model
 import numpy as np
 from PIL import Image
 
-lasses = ["monkey", "boar", "crow"]
+classes = ["monkey", "boar", "crow"]
 num_classes = len(classes)
 image_size = 50
 
@@ -26,44 +26,42 @@ def allowed_file(filename):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-  if request.method == 'POST':
-    if 'file' not in request.files:
-      flash('ファイルがありません')
-      return redirect(request.url)
-    file = request.files['file']
-    if file.filename == '':
-      flash('ファイルがありません')
-      return redirect(request.url)
-    if file and allowed_file(file.filename):
-      filename = secure_filename(file.filename)
-      file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-      filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
 
-      model = load_model('animal_cnn_aug.h5')
+            model = load_model('animal_cnn_aug.h5')
 
-      image = Image.open(filepath)
-      image = image.convert('RGB')
-      image = image.resize((image_size, image_size))
-      data = np.asarray(image)
-      X = []
-      X.append(data)
-      X = np.array(X)
+            image = Image.open(filepath)
+            image = image.convert('RGB')
+            image = image.resize((image_size, image_size))
+            data = np.asarray(image)
+            X = []
+            X.append(data)
+            X = np.array(X)
 
-      result = model.predict([X])[0]
-      predicted = result.argmax()
-      percentage = int(result[predicted] * 100)
+            result = model.predict([X])[0]
+            predicted = result.argmax()
+            percentage = int(result[predicted] * 100)
 
-      return "ラベル： " + classes[predicted] + ", 確率：" + str(percentage) + " %"
+            return "ラベル： " + classes[predicted] + ", 確率：" + str(percentage) + " %"
 
-      # return redirect(url_for('uploaded_file', filename=filename))
-  name = "Flask"
-  return render_template("index.html", name_value=name)
+            # return redirect(url_for('uploaded_file', filename=filename))
+    name = "Flask"
+    return render_template("index.html", name_value=name)
 
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
-  return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
